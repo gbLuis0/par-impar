1f614c8521c9296b0e656eb72eea08d4455c4bc0
Spyware <gabrielluisdm13@gmail.com>
2024-08-17 22:40:28 -0300

    organização das cores

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from random import randint, choice
+from colorama import Fore, Style
 
-verd = '\033[1;32m'
-roxo = '\033[1;35m'
-f = '\033[m'
-per = '\033[37;7m'
+ng = Style.BRIGHT
+verd = ng + Fore.GREEN
+roxo = ng + Fore.MAGENTA
+f = Style.RESET_ALL
+cor_pergunta = '\033[37;7m'
 
-cores = '\033[1;31m', verd, '\033[1;33m', '\033[1;34m', roxo, '\033[1;36m'
+cores = ('\033[1;31m', verd, '\033[1;33m', '\033[1;34m', roxo, '\033[1;36m')
 dec = 's'
 banner = open('banner.txt', 'r').read()
+
 while dec == 's':
     c = 0
     while True:
@@ -18,8 +21,8 @@ while dec == 's':
         cor = choice(cores)
 
         print(banner)
-        n = int(input(f'{per}Digite o seu número:{f} '))
-        p = input(f'{per}Par ou Ímpar? [P/I]{f} ').strip().upper()[0]
+        n = int(input(f'{cor_pergunta}Digite o seu número:{f} '))
+        p = input(f'{cor_pergunta}Par ou Ímpar? [P/I]{f} ').strip().upper()[0]
         ppc = 'Ímpar' if 'P' in p else 'Par'
         s = n + npc
 
@@ -34,6 +37,6 @@ while dec == 's':
         
         input('\nenter para continuar')
 
-    dec = input(f'{per}Deseja continuar? [s/n]{f} ').strip().lower()[0]
+    dec = input(f'{cor_pergunta}Deseja continuar? [s/n] ').strip().lower()[0]
 print('\x1b[2J\x1b[1;1H')
 print(f'{roxo}Volte sempre ;){f}')
