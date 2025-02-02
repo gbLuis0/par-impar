78fc8f43df9f37a9c7357288ad882bf9a7b4f25e
Spyware <gabrielluisdm13@gmail.com>
2025-02-02 12:29:22 -0300

    Update variables
    Refactoring variables

--- main.py
+++ main.py
@@ -1,36 +1,41 @@
 from random import randint, choice
 from colors import coloric_text, question_color, purple, green
 
-dec = 's'
+keep_playing = 's'
 banner = open('banner.txt', 'r').read()
 
-while dec == 's':
-    c = 0
+while keep_playing == 's':
+    win_count = 0
+
     while True:
         print('\x1b[2J\x1b[1;1H')
-        n = 0
-        ppc = p = ''
-        npc = randint(0, 10)
+        number = 0
+        pair_odd_pc = pair_odd = ''
 
         print(banner)
-        n = int(input(coloric_text('Digite o seu número: ', question_color))) 
-        p = input(coloric_text('Par ou Ímpar? [P/I] ', question_color)).strip().upper()[0]
-        ppc = 'Ímpar' if 'P' in p else 'Par'
-        s = n + npc
+        # player options
+        number = int(input(coloric_text('Digite o seu número: ', question_color)))
+        pair_odd = input(coloric_text('Par ou Ímpar? [P/I] ', question_color)).strip().upper()[0]
 
-        print(coloric_text('Opções do pc:', purple), ppc, npc)
+        # pc options
+        pair_odd_pc = 'Ímpar' if 'P' in pair_odd else 'Par'
+        pc_number = randint(0, 10)
 
-        if (s % 2 == 0 and p == 'P') or (s % 2 != 0 and p == 'I'):
+        sum = number + pc_number
+
+        print(coloric_text('Opções do pc:', purple), pair_odd_pc, pc_number)
+
+        if (sum % 2 == 0 and pair_odd == 'P') or (sum % 2 != 0 and pair_odd == 'I'):
             coloric_text('Você ganhou!!!', green, True)
-            c += 1
+            win_count += 1
         else:
             coloric_text('Ops, o computador ganhou de vc...', '\033[1;31m', True)
-            coloric_text(purple, 'Você ganhou %d vezes!' % c, True)
+            coloric_text(purple, 'Você ganhou %d vezes!' % win_count, True)
             break
         
         input('\nenter para continuar')
 
-    dec = input(coloric_text('Deseja continuar? [s/n] ', question_color)).strip().lower()[0]
+    keep_playing = input(coloric_text('Deseja continuar? [s/n] ', question_color)).strip().lower()[0]
 
 print('\x1b[2J\x1b[1;1H')
 coloric_text('Volte sempre ;)', purple, True)
